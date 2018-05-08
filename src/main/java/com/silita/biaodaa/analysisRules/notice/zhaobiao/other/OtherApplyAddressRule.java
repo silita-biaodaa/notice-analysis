7090bcd35ab2b31c716db938cc6e68965411c629
ddd0401 <ddd0401110@sina.com>
2018-05-08 16:23:39 +0800

    #标大大解析系统 报名地址—规则实现分类，配置化。

--- src/main/java/com/silita/biaodaa/analysisRules/notice/zhaobiao/other/OtherApplyAddressRule.java
+++ src/main/java/com/silita/biaodaa/analysisRules/notice/zhaobiao/other/OtherApplyAddressRule.java
@@ -8,7 +8,6 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -31,17 +30,21 @@ public class OtherApplyAddressRule implements SingleFieldAnalysis {
         return analyzeApplyAddress(segment);
     }
 
-    private static String[] headStrings = {"报名地点","报名地址","报名要求","投标报名","申请报名",
-            "获取招标文件时间、地点及售价 ","报名及发售招标文件",
-            "报名时间及地址","报名方式，时间及地点","报名方式与地点","报名起止时间",
-            "招标文件获取地点", "招标文件的获取","报名方式及要求","报名及招标文件的获取",
-            "报名及出售招标文件时间地点和要求","投标报名及招标文件发布",
-            "招标文件的获取时间、地址","报名及招标文件发售办法",
-            "报名和电子招标文件获取","招标文件发售",
-            "投标文件的递交","投标文件和保证金的递交",
-            "投标文件递交截止时间（开标时间）及地点","开标时间和地点",
-            "网上报名及招标文件的获取", "下载或查阅招标文件","下载获取招标文件",
-            "下载/获取招标文件","下载[(]获取[)]招标文件及图纸","下载招标文件","获取方式"};
+
+
+//    private static String[] headStrings = {"报名地点","报名地址","报名要求","投标报名","申请报名",
+//            "获取招标文件时间、地点及售价 ","报名及发售招标文件",
+//            "报名时间及地址","报名方式，时间及地点","报名方式与地点","报名起止时间",
+//            "招标文件获取地点", "招标文件的获取","报名方式及要求","报名及招标文件的获取",
+//            "报名及出售招标文件时间地点和要求","投标报名及招标文件发布",
+//            "招标文件的获取时间、地址","报名及招标文件发售办法",
+//            "报名和电子招标文件获取","招标文件发售",
+//            "投标文件的递交","投标文件和保证金的递交",
+//            "投标文件递交截止时间（开标时间）及地点","开标时间和地点",
+//            "网上报名及招标文件的获取", "下载或查阅招标文件","下载获取招标文件",
+//            "下载/获取招标文件","下载[(]获取[)]招标文件及图纸","下载招标文件","获取方式"};
+
+
 
     /**
      * 解析报名地址
@@ -51,75 +54,74 @@ public class OtherApplyAddressRule implements SingleFieldAnalysis {
     public String analyzeApplyAddress(String html) {
         String address = null;
         String onLineApply = "网上报名";
-        List<String> outerRegexList = new ArrayList<String>(headStrings.length);
-        for(int i=0;i<headStrings.length;i++){
-            StringBuffer outerRegex = new StringBuffer("(");
-            outerRegex.append(headStrings[i]).append(")").append(".{0,500}");//匹配head字符后1000个字符
-            outerRegexList.add(outerRegex.toString());
-        }
+        Map<String ,List<Map<String, Object>>> regListMap = commonService.queryRegexMapByField("applyAddress");
+        if(regListMap!=null && regListMap.keySet().size()>0){
+            List<Map<String, Object>> rangeList = regListMap.get("range");//范围匹配规则
+            List<Map<String, Object>> accurateList = regListMap.get("accurate");//精准匹配规则
+            List<Map<String, Object>> filterResultRegList= regListMap.get("filterResult");//结果过滤规则
+            List<Map<String, Object>> onlineList= regListMap.get("online");//在线报名业务
 
-        outerMtr: for(String outerRegex:outerRegexList) {
-            Pattern outerPtn = Pattern.compile(outerRegex.toString(),Pattern.CASE_INSENSITIVE);
-            Matcher outerMtr = outerPtn.matcher(html);
-            String matchPart = null;
+            //范围匹配
+            outerMtr: for(Map rangeMap:rangeList) {
+                String rangeRegex=(String)rangeMap.get("regex")+rangeMap.get("ext_content");
+                Pattern outerPtn = Pattern.compile(rangeRegex,Pattern.CASE_INSENSITIVE);
+                Matcher outerMtr = outerPtn.matcher(html);
+                String matchPart = null;
 
-            while (outerMtr.find()) {
-                matchPart = outerMtr.group();
-                //仅只匹配出表达式的字符段跳过
-                if(MyStringUtils.isNull(matchPart.replace(outerMtr.group(1),"").trim())){
-                    continue;
-                }
-
-                //网上报名解析
-                String onlineRegex = "((CA数字证书|登录电子化平台|数字证书登录|电子招标投标交易平台|网上支付)\\S?).*(报名|下载招标文件|交易平台|网上支付)*[。，,.]+";
-                String onlineRegex2 = "(下载.0,10}招标文件|信息网上报名|网上报名|网上进行电子报名|网上报名并获取电子招标文件|网上获取电子招标文件及其它招标资料)|((公共)+(资源)?(交易|网上)+(中心)?(网站|平台|系统)+)";
-                List<String > onlineList = new ArrayList<String>();
-                onlineList.add(onlineRegex);
-                onlineList.add(onlineRegex2);
-
-                for(String onLineRgx: onlineList) {
-                    Pattern onlinePtn = Pattern.compile(onLineRgx, Pattern.CASE_INSENSITIVE);
-                    if (onlinePtn.matcher(matchPart).find()) {
-                        address = onLineApply;
-                        logger.info( "\n@@@线上报名解析结果:[address:" + address + "] by [outerRegex:"+outerRegex+"][innerRegex:" + onLineRgx + "]\n@@@被解析片段：" + matchPart );
-                        break outerMtr;
+                while (outerMtr.find()) {
+                    matchPart = outerMtr.group();
+                    //仅只匹配出表达式的字符段跳过
+                    if(MyStringUtils.isNull(matchPart.replace(outerMtr.group(1),"").trim())){
+                        continue;
                     }
-                }
 
-                //解析详细地址
-                List<Map<String, Object>> arList = commonService.queryRegexByField("applyAddress");
-                for (int i = 0; i < arList.size(); i++) {
-                    Map map = arList.get(i);
-                    String innerRegex = map.get("regex").toString();
-                    Pattern innerPtn = Pattern.compile(innerRegex,Pattern.CASE_INSENSITIVE);
-                    Matcher innerMtr = innerPtn.matcher(matchPart);
-
-                    logger.debug(html + "\n[regex:" + innerRegex + "][groupCount:" + innerMtr.groupCount() + "]");
-                    while (innerMtr.find()) {
-                        int gCount = innerMtr.groupCount();
-                        if (innerMtr.groupCount() > 2) {
-                            try {
-                                address = innerMtr.group().replaceFirst(innerMtr.group(1), "").replaceFirst(innerMtr.group(gCount), "");//去掉首尾
-                            }catch (Exception e){
-//                                logger.error(e.getMessage(),e);
-                                address = innerMtr.group().replaceFirst(innerMtr.group(1), "");
-                            }
-                        } else if (innerMtr.groupCount() > 1) {
-                            address = innerMtr.group().replaceFirst(innerMtr.group(1), "");//去掉首
-                        } else {
-                            address = innerMtr.group();
-                        }
-                        address = filterAnalysisResult(address);
-                        //            logger.debug("解析段："+html+"\n[解析值："+address+"][regex:"+regex+"][groupCount:"+m.groupCount()+"][address:"+address+"]");
-                        if (MyStringUtils.isNotNull(address)&& address.length()>4) {
-                            logger.info( "\n##线下报名地址解析结果:[address:" + address + "] by [outerRegex:\""+outerRegex+"\"][innerRegex:\"" + innerRegex + "\"][groupCount:" + innerMtr.groupCount() + "]\n##被解析片段：" + matchPart );
+                    //网上报名解析
+                    for(Map onLineMap: onlineList) {
+                        String onLineRgx = (String)onLineMap.get("regex");
+                        Pattern onlinePtn = Pattern.compile(onLineRgx, Pattern.CASE_INSENSITIVE);
+                        if (onlinePtn.matcher(matchPart).find()) {
+                            address = onLineApply;
+                            logger.info( "\n@@@线上报名解析结果:[address:" + address + "] by [rangeRegex:"+rangeRegex+"][innerRegex:" + onLineRgx + "]\n@@@被解析片段：" + matchPart );
                             break outerMtr;
                         }
                     }
+
+                    //精准匹配
+                    for (Map accurateMap:accurateList) {
+                        String innerRegex = accurateMap.get("regex").toString();
+                        Pattern innerPtn = Pattern.compile(innerRegex,Pattern.CASE_INSENSITIVE);
+                        Matcher innerMtr = innerPtn.matcher(matchPart);
+
+                        logger.debug(html + "\n[regex:" + innerRegex + "][groupCount:" + innerMtr.groupCount() + "]");
+                        while (innerMtr.find()) {
+                            int gCount = innerMtr.groupCount();
+                            if (innerMtr.groupCount() > 2) {
+                                try {
+                                    address = innerMtr.group().replaceFirst(innerMtr.group(1), "").replaceFirst(innerMtr.group(gCount), "");//去掉首尾
+                                }catch (Exception e){
+//                                logger.error(e.getMessage(),e);
+                                    address = innerMtr.group().replaceFirst(innerMtr.group(1), "");
+                                }
+                            } else if (innerMtr.groupCount() > 1) {
+                                address = innerMtr.group().replaceFirst(innerMtr.group(1), "");//去掉首
+                            } else {
+                                address = innerMtr.group();
+                            }
+                            address = filterAnalysisResult(address,filterResultRegList);
+                            //            logger.debug("解析段："+html+"\n[解析值："+address+"][regex:"+regex+"][groupCount:"+m.groupCount()+"][address:"+address+"]");
+                            if (MyStringUtils.isNotNull(address)&& address.length()>4) {
+                                logger.info( "\n##线下报名地址解析结果:[address:" + address + "] by [rangeRegex:\""+rangeRegex+"\"][innerRegex:\"" + innerRegex + "\"][groupCount:" + innerMtr.groupCount() + "]\n##被解析片段：" + matchPart );
+                                break outerMtr;
+                            }
+                        }
+                    }
                 }
+                matchPart=null;
             }
-            matchPart=null;
+
         }
+
+
 
 //        if(MyStringUtils.isNull(address)){
 //            address ="线下报名";
@@ -127,19 +129,25 @@ public class OtherApplyAddressRule implements SingleFieldAnalysis {
         return address;
     }
 
-    private String filterAnalysisResult(String str){
-        String regex = "(<.+>|[。，,.；：]*$|[ ]*$|^[ ]*|<.?[^>]?p>)";
-        if(MyStringUtils.isNotNull(str)){
-            Pattern ptn = Pattern.compile(regex,Pattern.CASE_INSENSITIVE);
-            Matcher matcher = ptn.matcher(str);
-            while (matcher.find()) {
-                str = str.replaceAll(matcher.group(),"");
-            }
-            return str;
-        }else{
+    /**
+     * 根据过滤规则集合，过滤匹配字符
+     * @param str
+     * @param filterResultRegList
+     * @return
+     */
+    private String filterAnalysisResult(String str,List<Map<String, Object>> filterResultRegList){
+        if(MyStringUtils.isNull(str)){
             return null;
         }
-
+        for(Map regexMap: filterResultRegList) {
+            String regex =  (String)regexMap.get("regex");
+            Pattern ptn = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+            Matcher matcher = ptn.matcher(str);
+            while (matcher.find()) {
+                str = str.replaceAll(matcher.group(), "");
+            }
+        }
+        return str;
     }
 
 
--- src/main/java/com/silita/biaodaa/service/CommonService.java
+++ src/main/java/com/silita/biaodaa/service/CommonService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -24,5 +26,36 @@ public class CommonService {
     public List<Map<String, Object>> queryRegexByField( String field){
         logger.debug("###查詢數據庫queryRegexByField:"+field);
         return commonMapper.queryRegexByField(field);
+    }
+
+    @Cacheable(value = "regexCache", key="#field+'Map'")
+    public Map<String ,List<Map<String, Object>>> queryRegexMapByField( String field){
+        logger.debug("###查詢數據庫queryRegexByField:"+field);
+        return sortRegList(commonMapper.queryRegexByField(field));
+    }
+
+    /**
+     * 根据reg_type对表达式进行分类
+     * @param regList
+     * @return
+     */
+    private Map<String ,List<Map<String, Object>>> sortRegList(List<Map<String, Object>> regList){
+        Map<String ,List<Map<String, Object>>> regListMap=new HashMap<String ,List<Map<String, Object>>>();
+        if(regList!=null && regList.size()>0) {
+            List<String> typeList = new ArrayList<>();
+            for (Map record : regList) {
+                String reg_type = (String) record.get("reg_type");
+                if (typeList.contains(reg_type)) {
+                    regListMap.get(reg_type).add(record);
+                } else {
+                    List<Map<String, Object>> targetList = new ArrayList<Map<String, Object>>();
+                    targetList.add(record);
+                    regListMap.put(reg_type, targetList);
+                    typeList.add(reg_type);
+                }
+            }
+            typeList = null;
+        }
+        return regListMap;
     }
 }
